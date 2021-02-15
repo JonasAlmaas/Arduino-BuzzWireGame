144c122763d93ee2533ef10189bfd97931a28ab4
JonasAlmaas <44512227+JonasAlmaas@users.noreply.github.com>
2021-02-15 22:23:48 +0100

    Added Display Manager
    Added the display manager to take care of the display.

--- game/buzzWireGame.ino
+++ game/buzzWireGame.ino
@@ -41,6 +41,77 @@ void TimeManager::start() {
 float TimeManager::getTime() {
     return (millis() - timeStart) / 1000.0;
 }
+class DisplayManager {
+    public:
+        void setup();
+        void setBackgroundColourWhite();
+        void setBackgroundColourRed();
+        void displayLiveScore(float time, int touchCount);
+        void displayScoreScreen(float score, float bestScore);
+        DisplayManager();
+    private:
+        void clearDisplay();
+        void clearLine(int line);
+        rgb_lcd lcd;
+        float refreshRate;
+        float lastRefresh;
+};
+
+DisplayManager::DisplayManager() {
+    lcd = rgb_lcd();
+    refreshRate = 10;
+    lastRefresh = 0;
+}
+// This needs to be called in the setup, therefor it can't be in the constructor
+void DisplayManager::setup() {
+    lcd.begin(16, 2);
+    // This is just dumb, but staying
+    lcd.setRGB(0, 255, 0);
+    lcd.print("LOADING...");
+    delay(1000);
+    lcd.setCursor(0, 1);
+    lcd.print("Not realy though");
+    delay(1000);
+}
+void DisplayManager::setBackgroundColourWhite() {
+    lcd.setRGB(200, 200, 255);
+}
+void DisplayManager::setBackgroundColourRed() {
+    lcd.setRGB(255, 0, 0);
+}
+// A cheaper way of clearing the display
+void DisplayManager::clearDisplay() {
+    clearLine(0);
+    clearLine(1);
+}
+void DisplayManager::clearLine(int line) {
+    for (int i = 0; i > 16; i++) {
+        lcd.setCursor(i, line);
+        lcd.print(" ");
+    }
+}
+void DisplayManager::displayLiveScore(float time, int touchCount) {
+    if (millis() / 1000.0 - lastRefresh > (1 / refreshRate)) {
+        lastRefresh = millis() / 1000.0;
+
+        clearDisplay();
+        lcd.setCursor(0, 0);
+        lcd.print("Time:");
+        lcd.print(time);
+        lcd.setCursor(0, 1);
+        lcd.print("Touches:");
+        lcd.print(touchCount);
+    }
+}
+void DisplayManager::displayScoreScreen(float score, float bestScore) {
+    clearDisplay();
+    lcd.setCursor(0, 0);
+    lcd.print("Your Score:");
+    lcd.print(score);
+    lcd.setCursor(0, 1);
+    lcd.print("Best Score:");
+    lcd.print(bestScore);
+}
 
 void setup() {
 
