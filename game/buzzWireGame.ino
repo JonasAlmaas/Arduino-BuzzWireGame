9c5e24ee62c20ba845e652f7ca6ac48e75fe37d3
JonasAlmaas <44512227+JonasAlmaas@users.noreply.github.com>
2021-02-15 22:20:49 +0100

    Added Pin Manager
    Added the pin manager with functions to return all the pins

--- game/buzzWireGame.ino
+++ game/buzzWireGame.ino
@@ -1,6 +1,30 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
 
+class PinManager {
+    public:
+        int getPinTouch();
+        int getPinStart();
+        int getPinEnd();
+        int getPinBuzzer();
+        PinManager();
+    private:
+        int Touch;
+        int Start;
+        int End;
+        int Buzzer;
+};
+PinManager::PinManager() {
+    Touch = 13;
+    Start = 10;
+    End = 11;
+    Buzzer = 3;
+}
+int PinManager::getPinTouch() {return Touch;}
+int PinManager::getPinStart() {return Start;}
+int PinManager::getPinEnd() {return End;}
+int PinManager::getPinBuzzer() {return Buzzer;}
+
 void setup() {
 
 }
